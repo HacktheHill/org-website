d16ea2ee354ed2bdace55414c58e9e616789c949
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-06-05 19:07:09 -0400

    feat: cms connection established

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,24 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
 import tailwind from "@astrojs/tailwind";
+import sanity from "@sanity/astro";
 const DEV = process.argv[2] === "build" ? false : true;
 
 // https://astro.build/config
 export default defineConfig({
 	outDir: "build",
 	site: DEV ? "http://localhost:4321" : "https://hackthehill.com",
-	integrations: [react(), sitemap(), partytown(), tailwind()],
+	integrations: [
+		react(),
+		sitemap(),
+		partytown(),
+		tailwind(),
+		sanity({
+			projectId: "j9zfvetu",
+			dataset: "production",
+			useCdn: false,
+			apiVersion: "2024-06-05",
+		}),
+	],
 	toolbar: false,
 });
--- src/components/Blog/BlogPage.jsx
+++ src/components/Blog/BlogPage.jsx
@@ -1,12 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "../../global.css";
 import Construction from "../Construction/Construction";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import shape from "../../assets/patterns/ssshape.svg";
+
+import { sanityClient } from "sanity:client";
 
 export default function BlogPage() {
+	const [posts, setPosts] = useState([]);
+
 	useEffect(() => {
 		AOS.init({ once: false, duration: 700 });
+
+		sanityClient.fetch(`*[_type == "blog" && defined(slug)] | order(publishedAt desc)`).then(posts => {
+			setPosts(posts);
+		});
 	}, []);
-	return <Construction pageNotFound={false} />;
+
+	return (
+		<div className="flex justify-center items-center w-full h-screen bg-background-dark relative overflow-hidden">
+			<div className="flex flex-col w-10/12 h-full justify-center items-center gap-20 py-36 text-left max-w-2xl z-[1]">
+				<div className="flex flex-col items-start gap-10 p-8 rounded-3xl bg-blur-svg relative w-1/2 lg:w-full mt-32">
+					<h2 className="mt-8">Blog</h2>
+					<p>{`You are seeing ${posts.length} blog posts`}</p>
+					<ul>
+						{posts?.map(post => (
+							<li key={post.title}>
+								<a href={"/blog/" + post.slug.current} className="post-link">
+									{post.title}
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+			<img
+				src={shape.src}
+				alt="shape"
+				className="w-full max-w-bg-deco opacity-25 absolute z-[0] -translate-x-1/2 -translate-y-1/4"
+			/>
+			<img
+				src={shape.src}
+				alt="shape"
+				className="w-full max-w-bg-deco opacity-25 absolute z-[0] translate-x-1/2 translate-y-[10%] -scale-y-75 scale-x-75"
+			/>
+		</div>
+	);
 }
