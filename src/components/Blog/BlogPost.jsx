76ddbdf2314422a8f2d26406a486a163f6549c57
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-06-05 21:33:40 -0400

    chore: handling fallback for missing data

--- src/components/Blog/BlogPost.jsx
+++ src/components/Blog/BlogPost.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../global.css";
 import shape from "../../assets/patterns/ssshape.svg";
-
+import hth_fall_theme from "../../assets/SVGs/hth_fall_theme.svg";
 import { sanityClient } from "sanity:client";
 import imageUrlBuilder from "@sanity/image-url";
 
@@ -16,18 +16,21 @@ export default function BlogPost({ data }) {
 		<div className="flex justify-center items-center w-full h-screen bg-background-dark relative overflow-hidden">
 			<div className="flex flex-col w-10/12 h-full justify-center items-center gap-20 py-36 text-left max-w-2xl z-[1]">
 				<div className="flex flex-col items-start gap-10 p-8 rounded-3xl bg-[#020106] relative w-1/2 lg:w-full mt-32">
-					<h2 className="mt-8">{data.title}</h2>
-					<p>{data.publishedAt}</p>
-					<p>{data.author}</p>
+					<a href="/blog" className="post-link">
+						{"< Back"}
+					</a>
+					<h2 className="mt-8">{data?.title}</h2>
+					<p>{data?.publishedAt}</p>
+					<p>{data?.author ? data.author : "Unknown Author"}</p>
 					<div className="w-full">
-						<img src={urlFor(data.coverImage.asset).url()} width="10%" />
+						<img
+							src={data?.coverImage ? urlFor(data?.coverImage?.asset).url() : hth_fall_theme.src}
+							width="100px"
+						/>
 					</div>
-					{data.body.map((block, index) => {
+					{data.body?.map((block, index) => {
 						if (block._type === "block") {
-							return <p key={index}>{block.children[0].text}</p>;
-						}
-						if (block._type === "image") {
-							return <img key={index} src={block.asset.url} alt={block.alt} />;
+							return <p key={index}>{block?.children?.[0]?.text}</p>;
 						}
 						return null;
 					})}
