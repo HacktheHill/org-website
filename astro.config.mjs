686a6e0c1d2b9cdb9f7912985f1f595fb8374723
Daniel Thorp <arcanistzed@gmail.com>
2026-07-09 05:05:58 -0400

    chore: upgrade Astro Sanity React and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
-import tailwind from "@astrojs/tailwind";
 import sanity from "@sanity/astro";
+import tailwindcss from "@tailwindcss/vite";
 const DEV = process.argv[2] === "build" ? false : true;
 import { loadEnv } from "vite";
 const { SANITY_PROJECT_ID, SANITY_DATASET } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
@@ -16,7 +16,6 @@ export default defineConfig({
 		react(),
 		sitemap(),
 		partytown(),
-		tailwind(),
 		sanity({
 			projectId: SANITY_PROJECT_ID,
 			dataset: SANITY_DATASET,
@@ -24,5 +23,8 @@ export default defineConfig({
 			apiVersion: "2024-06-05",
 		}),
 	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 	toolbar: false,
 });
